ebe5e30a7923556660cac1eb9e857ee9829d2cd1
Mikkel <lmikkelc5@gmail.com>
2025-12-03 12:21:40 -0700

    add the individual data files

--- split_csv.py
+++ split_csv.py
@@ -1,2 +1,9 @@
 import pandas as pd
 
+big_df = pd.read_csv("senate_data_with_text.csv")
+session = [119, 118, 117, 116, 115, 114, 113, 112, 111, 110, 109, 108, 107, 106, 105, 104]
+
+for item in session:
+    df = big_df[big_df["session"] == item]
+    df.to_csv(f"data/session_{item}.csv", index=False)
+    print(f'saved session {item}')
