5bc04221888c485fc524329106461a2590b7ae20
Mikkel <lmikkelc5@gmail.com>
2025-12-13 20:43:33 -0700

    update file path in app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -5,5 +5,5 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 import streamlit as st
 from src.data import load_processed
 
-df = load_processed("data/processed/hearings.csv")
+df = load_processed("data/cleaned/session_119_cleaned.csv")
 st.dataframe(df)
